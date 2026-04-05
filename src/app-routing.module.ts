95cc73752dbfb120ea1b23ae32ae67eb2133c9b3
marcelino26-hub <marcelinogame26@gmail.com>
2026-04-05 19:16:00 -0300

    Update app-routing.module.ts

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,1 +1,12 @@
-
+{
+  path: 'atendimento',
+  loadChildren: () => import('./servicos/atendimento/atendimento.module').then(m => m.AtendimentoPageModule)
+}
+{
+  path: 'relatorio',
+  loadChildren: () => import('./servicos/relatorio/relatorio.module').then(m => m.RelatorioPageModule)
+},
+{
+  path: 'senha',
+  loadChildren: () => import('./servicos/senha/senha.module').then(m => m.SenhaPageModule)
+}
