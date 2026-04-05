84265af590922da456ad06f95d1e8761b95d5562
marcelino26-hub <marcelinogame26@gmail.com>
2026-04-05 19:16:40 -0300

    Update app-routing.module.ts

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,12 +1,28 @@
-{
-  path: 'atendimento',
-  loadChildren: () => import('./servicos/atendimento/atendimento.module').then(m => m.AtendimentoPageModule)
-}
-{
-  path: 'relatorio',
-  loadChildren: () => import('./servicos/relatorio/relatorio.module').then(m => m.RelatorioPageModule)
-},
-{
-  path: 'senha',
-  loadChildren: () => import('./servicos/senha/senha.module').then(m => m.SenhaPageModule)
-}
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'atendimento',
+    loadChildren: () => import('./servicos/atendimento/atendimento.module').then(m => m.AtendimentoPageModule)
+  },
+  {
+    path: 'relatorio',
+    loadChildren: () => import('./servicos/relatorio/relatorio.module').then(m => m.RelatorioPageModule)
+  },
+  {
+    path: 'senha',
+    loadChildren: () => import('./servicos/senha/senha.module').then(m => m.SenhaPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'atendimento',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
